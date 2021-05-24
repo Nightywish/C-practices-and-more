37c780619bb5fc42c230e83939057de11db13392
Nightywish <chunyu9655@hotmail.com>
2021-05-24 15:13:34 -0400

    for future use

--- C++ practices and more.cpp
+++ C++ practices and more.cpp
@@ -1,13 +1,17 @@
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-  
-
-
-
+	int exponent, base;
+	cout << "what is the base?" << endl; 
+		cin >> base; //user input 
+	cout << "what is the exponent?"<<endl;
+		cin >> exponent;// user input exponent
+	cout << "the result you are looking for is " << pow(base, exponent) << endl; //output
+	return 0;
 
 
 
