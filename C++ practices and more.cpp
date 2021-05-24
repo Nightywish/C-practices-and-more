7c78ccdf646bb8f572ea4103fd14502899f813ca
Nightywish <chunyu9655@hotmail.com>
2021-05-24 16:00:33 -0400

    two ways of doing functions

--- C++ practices and more.cpp
+++ C++ practices and more.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+/*
 int main()
 {
 	int exponent, base;
@@ -16,4 +16,28 @@ int main()
 
 
 
+}
+
+*/
+ //declaration
+double power(double base, double exponent)
+{
+	double result = 1;
+	for (int i = 0; i < exponent; i++)
+	{
+		result = result * base;
+	}
+	return result;
+}
+int main()
+{
+
+	int exponent, base;
+	cout << "what is the base?" << endl;
+	cin >> base; //user input 
+	cout << "what is the exponent?" << endl;
+	cin >> exponent;// user input exponent
+	double Mypower = pow(base, exponent);
+	cout << "your result is " << Mypower << endl;
+	return 0;
 }
